0dbd2ae80a2b8edc21cdfc59485896d0e66e227b
William Yang <505741310@qq.com>
2016-09-20 19:40:52 +0800

    增加文本提示信息和中英文切换

--- tickets.py
+++ tickets.py
@@ -1,6 +1,6 @@
 #coding:utf-8
 #Author: William Yang
-#Version: V1.0
+#Version: V1.1
 
 
 """
@@ -16,9 +16,10 @@ Options:
     -t        特快
     -k        快速
     -z        直达
-
 """
 
+import os
+import xpinyin
 import colorama
 import requests
 from docopt import docopt
@@ -28,26 +29,47 @@ from prettytable import PrettyTable
 
 def cli():
     """Command line interface"""
+    global debug
+    global language
+    
+    if language.upper() == "CN":
+        print("查询中, 请耐心等待...")
+    elif language.upper() == "EN":
+        print("Querying, please be patient...")
+    
     arguments = docopt(__doc__)
+    if debug:
+        print(arguments)
     from_station = stations.get(arguments['<from>'])
     to_station = stations.get(arguments['<to>'])
     date = arguments['<date>']
        
     url = "https://kyfw.12306.cn/otn/leftTicket/queryT?leftTicketDTO.train_date={}&leftTicketDTO.from_station={}&leftTicketDTO.to_station={}&purpose_codes=ADULT".format(date, from_station, to_station)
-    
+
     r = requests.get(url, verify = False)
-    rows = r.json()['data']
-    #pprint(rows)
-    trains = TrainCollection(rows)
-    trains.pretty_print()
+    try:
+        rows = r.json()['data']
+        if debug:
+            pprint(rows)
+        trains = TrainCollection(rows)
+        trains.pretty_print()        
+    except(KeyError):
+        print("\n你的输入有误, 无法查询, 请重新更改条件后搜索\n")
+    except(Exception) as e:
+        print("服务器出错, 无法查询")
+        if debug:
+            print(e)
+    
+    
 
 
 class TrainCollection():
-    header = "train station time duration first second softsleep hardsleep hardsit remark".split()
-    
+    header_EN = "train station time duration first second softsleep hardsleep hardsit remark".split()
+    header_CN = "车次 站点 时间 历时 一等座 二等座 软卧 硬卧 硬座 备注".split()
     
     def __init__(self, rows):
         self.rows = rows
+        self.trains_num = self._get_train_count(self.rows)
         
         
     def _get_duration(self, row):
@@ -57,21 +79,30 @@ class TrainCollection():
         if duration.startswith('00'):
             return duration[3:]
         if duration.startswith('0'):
-            return duration[1:]
+            return '0' + duration[1:]
         return duration
+    
+    
+    def _get_train_count(self, rows):
+        """获取车次数量"""
+        count = len(self.rows)
+        return count
     
     
     @property
     def trains(self):
+        index = 0  #记录循环中当前车次序号
         for row in self.rows:
+            #列车信息在'queryLeftNewDTO'字典对应的值里面
             info = row['queryLeftNewDTO']
+            #如果'controlled_train_flag'为1, 代表该车次停运
             if info['controlled_train_flag'] == '1':
                 train = [
                     
-                    info['station_train_code'] + '\n' * 2,
+                    info['station_train_code'],
                     
                     '\n'.join([info['from_station_name'], info['to_station_name']]),
-                                    
+                    
                     '-', 
                     
                     '-',
@@ -92,7 +123,7 @@ class TrainCollection():
             else:
                 train = [
                     
-                    info['station_train_code'] + '\n' * 2,
+                    info['station_train_code'],
                     
                     '\n'.join([info['from_station_name'], info['to_station_name']]),
                     
@@ -113,16 +144,44 @@ class TrainCollection():
                     '可预订', 
                     
                 ]
+            #为保持整齐与美观, 尾行不用加多余空位
+            if not index == self.trains_num - 1:
+                train[0] += '\n\n'
+            #序号加一
+            index += 1
                 
             yield train
             
             
     def pretty_print(self):
-        pt = PrettyTable(self.header)
-        for train in self.trains:
-            pt.add_row(train)
-            #pt.add_row(('-------- ' * 10).split())
-        print(pt)
+        global language
+        if self.trains_num == 0:
+            if language.upper() == "CN":
+                print("\n没有查询到符合条件的车次信息, 请重新设定条件查询\n")
+            elif language.upper() == "EN":
+                print("\nCan not find any train meet the specific conditions, "
+                      "please query again with apposite condition\n")
+        else:
+            if language.upper() == "CN":
+                print("\n共查询到 {} 车次, 详细信息如下:\n".format(self.trains_num))
+                pt = PrettyTable(self.header_CN)
+                for train in self.trains:
+                    pt.add_row(train)
+                print(pt)
+            elif language.upper() == "EN":
+                print("\nGet {} trains totally and the detail as follow:\n".format(self.trains_num))
+                pt = PrettyTable(self.header_EN)
+                for train in self.trains:
+                    pt.add_row(train)
+                print(pt)                
+                
+
 
 if __name__ == '__main__':
+    debug = False
+    language = "CN"
+    if language.upper() == "CN":
+        print("TITLE 12306网站火车余票查询工具 Python专版 V1.0\n")
+    elif language.upper() == "EN":
+        print("TITLE 12306 website TRAIN TICKET QUERY TOOL based on Python V1.0\n")
     cli()
